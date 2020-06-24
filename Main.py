209d2fab4996a58e5d385d32862a2195940e4498
Nabil <nkoney14@outlook.com>
2020-06-24 16:33:58 -0400

    Edited Previously made comments

--- Main.py
+++ Main.py
@@ -9,12 +9,12 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 
 #Initializing model
-model = Sequential() #sequential represents the layer we are making and what type of layer it is
+model = Sequential() #sequential represents the layers we are making and what of type
 
-#Layers: Convulutional, Max Pooling, Dense (2), Dropout
+#Layers: Convulutional, Max Pooling, Dense, Dropout
 conv_layer = Conv2D(filter=32, kernel_size=(3, 3), activation='relu')
 max_pool_layer = MaxPooling2D(pool_size=(2, 2))
-dense_layer = Dense(units=1024, activation='softmax')
+dense_layer = Dense(units=10, activation='softmax')
 dropout_layer = Dropout(rate=0.5)
 
 #Adding layers to model
